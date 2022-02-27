3b15c64a50f7b1ea28e5f703f85c9659f1e650f1
Being-Insane-HD <hridehd007@gmail.com>
2022-02-27 17:43:49 +0530

    ain

--- src/app/dab-login/dab-login.component.ts
+++ src/app/dab-login/dab-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../model/user';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-dab-login',
@@ -8,11 +10,34 @@ import { Router } from '@angular/router';
 })
 export class DabLoginComponent implements OnInit {
 
+  user = new User();
+  msg="";
+
   constructor(
+    private userService: UserService,
     public router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  loginUser(){
+    this.userService.loginUserFromRemote(this.user).subscribe(
+      (data: User) => {
+        console.log("response received");
+        console.log(data);
+        localStorage.setItem('userid', String(data.userId));
+        localStorage.setItem('username', data.userName);
+        localStorage.setItem('password', data.password);
+        localStorage.setItem('role', data.role);
+
+        this.router.navigate(['/dab-appoint'])
+      },
+      error => {
+        console.log("exception occured");
+        this.msg = "Bad Credential, Please enter valid user name and password";
+      }
+    );
   }
 
 }
